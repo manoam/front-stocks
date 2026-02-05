e4672d4fb23d8d60a3c0bd3750d07605e238d320
Manoa Fanantenana <manoarazafindrabe@gmail.com>
2026-02-05 19:45:21 +0300

    reglage keycloak

--- src/config/keycloak.ts
+++ src/config/keycloak.ts
@@ -2,7 +2,7 @@ import Keycloak from 'keycloak-js';
 
 const keycloakConfig = {
   url: import.meta.env.VITE_KEYCLOAK_URL || 'https://keycloak-production-9856.up.railway.app',
-  realm: import.meta.env.VITE_KEYCLOAK_REALM || 'stock-management',
+  realm: import.meta.env.VITE_KEYCLOAK_REALM || 'konitys',
   clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID || 'stock-management-client',
 };
 
